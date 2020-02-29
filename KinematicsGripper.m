aa34693372365703f1208586e098507d3eb1c0db
Gfycyd <31628809+Gfycyd@users.noreply.github.com>
2020-02-29 18:19:27 +0300

    KinematicsGripper adding graph

--- KinematicsGripper.m
+++ KinematicsGripper.m
@@ -1,120 +1,125 @@
 %% KINEMATICS OF SELECTED GRIPPER
 % 
-  
 clear
 close all
-syms w h OA AB BC CD DE FE t beta0 PC real;
-syms cX cY aY fX fY real;
+syms w real;
+
+%% 0 Step: What we have
+PC = 2;
+h = 1; % from 1 to 2 
+OA = 5; % that give us 
+AB = 5;
+BC  = AB;
+DE = AB;
+CD = DE; 
+aX0 = 0;
+
+
+cX = AB + sqrt(BC^2 - PC^2);
+cY = OA + PC;
+
+FE = 5; % we know that CD = FE and they are parallel, and also BC = CD
+beta0 = asin(PC/BC);
+
+
+fX = 12;
+fY = 2 ; % we think as an example
+
 %% 1 STEP
-disp('Angular Velocity we receive to Linear Velocity of Rod OA(half of AA1): ');
-Vbsp = simplify(w/2/pi * h);
-disp(Vbsp)
-%% 2nd Step
-AC = (AB+BC) - Vbsp*t ;
+aX = aX0 + simplify(w/2/3.1416 * h);
+aY = OA;
+
+AP = cX - aX;
+AC = simplify(sqrt(PC*PC + AP*AP));
+fi = simplify(asin(PC/AC));
 
 % cosinus theorem:
-alpha_beta_minus_180 = simplify(acos((AB*AB + BC*BC - AC*AC)/2/AB/BC) * 180 / pi);
-
+abc = simplify(acos((AB*AB + BC*BC - AC*AC)/2/AB/BC));
 % by sinus theorem
-ugolA = asin(AC/BC * sin(alpha_beta_minus_180)) * 180 /pi;
-ugolC = asin(AC/AB * sin(alpha_beta_minus_180)) * 180 /pi;
-fi = asin(PC/AC);
+ugolA = simplify(asin(BC/AC * sin(abc)));
+
 
 % angles alpha and beta
 alpha = ugolA - fi;
-beta = 180 - alpha_beta_minus_180 - alpha;
+beta = 3.1416 - abc - alpha;
+gamma = beta - beta0;
 
-% angle fi is equal beta-beta0
-fi = beta - beta0;
-gamma = fi; % there are parallel
 
-dX = simplify(cX + CD*cos(gamma));
-dY = simplify(cY + CD*sin(gamma));
-
-aX0 = 0;
-aX = aX0 + Vbsp*t;
-
-bX = simplify(aX - AC*sin(alpha));
-bY = simplify(aY - AC*sin(alpha));
-eY = simplify(fY + FE*cos(gamma));
-eX = simplify(fX + FE*cos(gamma));
-
+eY = fY + FE*sin(gamma);
+eX = fX + FE*cos(gamma);
 %% 3rd Step: Velocities and Accelerations for all points
-
-% velocity of Joint A
-vAY = diff(aY);
-vAX = diff(aX);
-vA = sqrt(vAX*vAX + vAY*vAY);
-
-% acceleration of Joint A
-aAY = diff(vAY);
-aAX = diff(vAX);
-aA = sqrt(aAX*aAX + aAY*aAY);
-
-
-% velocity of Joint B
-vBY = diff(bY);
-vBX = diff(bX);
-vB = sqrt(vBX*vBX + vBY*vBY);
-
-% acceleration of Joint B
-aBY = diff(vBY);
-aBX = diff(vBX);
-aB = sqrt(aBX*aBX + aBY*aBY);
-
-
-% velocity of Joint C
-vCY = diff(cY);
-vCX = diff(cX);
-vC = sqrt(vCX*vCX + vCY*vCY);
-
-% acceleration of Joint C
-aCY = diff(vCY);
-aCX = diff(vCX);
-aC = sqrt(aCX*aCX + aCY*aCY);
-
-
-% velocity of Joint D
-vDY = diff(dY);
-vDX = diff(dX);
-vD = sqrt(vDX*vDX + vDY*vDY);
-
-% acceleration of Joint D
-aDY = diff(vDY);
-aDX = diff(vDX);
-aD = sqrt(aDX*aDX + aDY*aDY);
-
 
 % velocity of Joint E
 vEY = diff(eY);
 vEX = diff(eX);
-vE = sqrt(vEX*vEX + vEY*vEY);
 
 % acceleration of Joint E
 aEY = diff(vEY);
 aEX = diff(vEX);
-aE = sqrt(aEX*aEX + aEY*aEY);
-
-
-% velocity of Joint F
-vFY = diff(fY);
-vFX = diff(fX);
-vF = sqrt(vFX*vFX + vFY*vFY);
-
-% acceleration of Joint F
-aFY = diff(vFY);
-aFX = diff(vFX);
-aF = sqrt(aFX*aFX + aFY*aFY);
 %% 4th Step: Show Values of Joint E (or we consider it as an endeffector
 
 disp("Print X pos of E:")
 disp(simplify(eX))
 
 disp("Print Y pos of E:")
-disp(simplify(eY))
+disp(eY)
 
-disp("Print velocity of E:")
-disp(vE)
+%disp("Print velocity of E:")
+%disp(vE)
 
-disp("Print acceleration of E:")
-disp(aE)
+%disp("Print acceleration of E:")
+%disp(aE)
+
+%% graphs: 
+
+w = 0;
+count = 0;
+
+eX1 = [];
+eY1 = [];
+
+eAX1 = [];
+eAY1 = [];
+
+eVX1 = [];
+eVY1 = [];
+
+for c = 0:130
+     w = c*0.09;
+
+     eX1 = [eX1 eval(eX)];
+     eY1 = [eY1 eval(eY)];
+     eVX1 = [eVX1 eval(vEX)];
+     eVY1 = [eVY1 eval(vEY)];
+     
+     eAX1 = [eAX1 eval(aEX)];
+     eAY1 = [eAY1 eval(aEY)];
+     
+     count = count + 1;
+end
+
+w = linspace(0,(count-1), count);
+
+figure(1)
+plot(w,eX1,'b');
+hold on;
+plot(w,eY1,'r');
+hold off;
+xlabel('angle, rad')
+ylabel('vX and vY of E')
+
+figure(2)
+plot(w,eVX1,'b');
+hold on;
+plot(w,eVY1,'r');
+hold off;
+xlabel('angle, rad')
+ylabel('vX and vY of E')
+
+figure(3)
+plot(w,eAX1,'b');
+hold on;
+plot(w,eAY1,'r');
+hold off;
+xlabel('angle, rad')
+ylabel('Accel of xE and yE')
